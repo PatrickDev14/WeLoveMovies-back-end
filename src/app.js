d48a9fc8bcc2e779f9280340f84c3546094417d3
Patrick Lauher <plauher14@gmail.com>
2022-07-29 17:25:18 -0500

    added route for GET /theaters
    Added logic in .service list query including reduce-properties.
    Completed route. In app.js, added theatersRouter logic, and final
    error handler.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 const asyncErrorBoundary = require("./errors/asyncErrorBoundary");
 
@@ -14,6 +15,7 @@ app.use(cors());
 // routes
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
 
 
 // errors
@@ -22,6 +24,13 @@ app.use(asyncErrorBoundary);
 // Not Found handler
 app.use((req, res, next) => {
   next({ status: 404, message: `Not found: ${req.originalUrl}` });
+});
+
+// Error handler
+app.use((error, req, res, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
